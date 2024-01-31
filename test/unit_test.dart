f43dffac5a346c7dea002caf1fb454cf272ca8fa
Hampus Hammarlund <hampus_hammarlund@hotmail.com>
2024-01-31 10:39:15 +0900

    Added test with file not in repo

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_actions_sandbox/download_options.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -7,6 +8,10 @@ void main() {
       expect(1 + 2, 3);
       expect(1 + 3, 4);
       expect(1 + 4, 5);
+    });
+
+    test('Secret test', () {
+      expect(DownloadOptions.downloadUrl, isNotNull);
     });
   });
 }
